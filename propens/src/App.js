4932b0c47b38cad7cafb86aee0978dc739912a2a
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-02 15:15:23 -0500

    Setup React Router and some basic routes

--- propens/src/App.js
+++ propens/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 import TopBar from './components/App/NavBars/TopBar';
 import BottomBar from './components/App/NavBars/BottomBar';
 import Dashboard from './components/Dashboard/DashBoard';
+import Registration from './components/User/Registration/Registration';
+import Login from './components/User/Login/Login';
 
 const App = () => {
     return (
         <>
         <TopBar />
-        <Dashboard />
+        <Switch>
+            <Route exact path ='/'>
+                <Dashboard />
+            </Route>
+            <Route path='/register'>
+                <Registration />
+            </Route>
+            <Route>
+                <Login />
+            </Route>
+        </Switch>
         <BottomBar />
       </>
     );
--- propens/src/components/App/NavBars/TopBar.js
+++ propens/src/components/App/NavBars/TopBar.js
@@ -1,9 +1,21 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const TopBar = () => {
   return (
     <div className="topbar">
       <h1>ProPens</h1>
+      <ul>
+        <li>
+          <Link to="/">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
     </div>
   );
 };
