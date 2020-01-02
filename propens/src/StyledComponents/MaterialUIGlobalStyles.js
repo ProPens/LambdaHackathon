15950b91caa6a441c8995d16cd361937c809b3a9
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-02 17:15:54 -0500

    Added MaterialUI styled components

---
+++ propens/src/StyledComponents/MaterialUIGlobalStyles.js
@@ -0,0 +1,20 @@
+import {makeStyles} from '@material-ui/core/styles';
+
+
+export const useStyles = makeStyles({
+    
+    root : {
+        margin: 0,
+        padding: 0
+    },
+    topNavigation: {
+        margin: '0 auto',
+        padding: '20px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        backgroundColor: 'black',
+        color: 'whitesmoke',
+        fontFamily: `"Kulim Park", sansSerif`,
+    }
+})
--- propens/src/components/Navigation/TopNav.js
+++ propens/src/components/Navigation/TopNav.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {useStyles} from '../../StyledComponents/MaterialUIGlobalStyles';
 
 const TopNav =  () =>{
+    const classes = useStyles();
     return(
-        <div className="top-navigation">
+        <div className={classes.topNavigation}>
       <h1>ProPens</h1>
       <ul>
         <li>
